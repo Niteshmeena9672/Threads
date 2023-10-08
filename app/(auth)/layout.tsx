9b38923c97a2ff20da1c406e5efeb313c88f072f
nitesh <niteshmeena9672@gmail.com>
2023-10-08 18:30:26 +0530

    Authentication completed

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,24 @@
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+export const metadata = {
+  title: "Threads",
+  description: "A Next.js 13 Meta Threads Application",
+};
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className} bg-dark-1`}>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
