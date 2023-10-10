292b8bd02d9a74643a96f93919dbfc3fb3f3ffd6
nitesh <niteshmeena9672@gmail.com>
2023-10-11 00:19:01 +0530

    Create different components for homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,11 @@ import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <div>
-      <UserButton afterSignOutUrl="/"/>
+      {/* This will give general tempelate of icon inbuilt in
+      clerk */}
+      {/* <UserButton afterSignOutUrl="/"/> */}
+
+      <h1 className="head-text text-left">Home</h1>
     </div>
   )
 }
