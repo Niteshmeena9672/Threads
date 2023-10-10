545918e7991ed0f26cc4882436638a4cee8ca88c
nitesh <niteshmeena9672@gmail.com>
2023-10-11 02:35:02 +0530

    right bar completed

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,20 +1,21 @@
-import { ClerkProvider } from "@clerk/nextjs";
-import "../globals.css";
+import React from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Topbar from "@/components/shared/Topbar";
-import RightSidebar from "@/components/shared/RightSidebar";
-import Bottombar from "@/components/shared/Bottombar";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+
+import "../globals.css";
 import LeftSidebar from "@/components/shared/LeftSidebar";
-
-
-export const metadata = {
-  title: "Threads",
-  description: "A Next.js 13 Meta Threads Application",
-};
-
+import Bottombar from "@/components/shared/Bottombar";
+import RightSidebar from "@/components/shared/RightSidebar";
+import Topbar from "@/components/shared/Topbar";
 
 const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Threads",
+  description: "A Next.js 13 Meta Threads application",
+};
 
 export default function RootLayout({
   children,
@@ -22,19 +23,25 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
-      <html lang="en">
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
+      <html lang='en'>
         <body className={inter.className}>
           <Topbar />
-          <main>
+
+          <main className='flex flex-row'>
             <LeftSidebar />
-            <section className="main-container">
-              <div className="w-full max-w-4xl">{children}</div>
+            <section className='main-container'>
+              <div className='w-full max-w-4xl'>{children}</div>
             </section>
+            {/* @ts-ignore */}
             <RightSidebar />
           </main>
+
           <Bottombar />
-          {children}
         </body>
       </html>
     </ClerkProvider>
--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const RightSidebar = () => {
   return (
-    <div>RightSidebar</div>
+    <section className="custom-scrollbar rightsidebar">
+      <div className="fkex flex-1 flex-col justify-start">
+        <h3 className='text-heading4-medium text-light-1'>Suggested Communites</h3>
+      </div>
+      <div className="fkex flex-1 flex-col justify-start">
+      <h3 className='text-heading4-medium text-light-1'>Suggested user</h3>
+      </div>
+    </section>
   )
 }
 
