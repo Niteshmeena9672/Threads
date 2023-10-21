258ed13148909f67f13a4597baba82bf8a66427d
nitesh <niteshmeena9672@gmail.com>
2023-10-21 11:11:53 +0530

    golbal.css and next config updated

--- next.config.js
+++ next.config.js
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    experimental: {
+        serverActionsTs: true,
+        serverComponentsExternalPackages: ['mongoose'],
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'img.clerk.com'
+            },
+            {
+                protocol: 'https',
+                hostname: 'img.clerk.dev'
+            },
+            {
+                protocol: 'https',
+                hostname: 'uploadthing.com'
+            },
+            {
+                protocol: 'https',
+                hostname :  'placehold.co'
+            }
+        ]
+    }
+}
 
 module.exports = nextConfig
