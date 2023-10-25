8b4bd28c2fd24a9eb38f1d4e3b1b9ae99b2306ac
nitesh <niteshmeena9672@gmail.com>
2023-10-25 11:30:28 +0530

    Database creation + database integration completed

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,23 +1,23 @@
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
-import { fetchUser } from "@/lib/actions/user.actions";
+// import { fetchUser } from "@/lib/actions/user.actions";
 import AccountProfile from "@/components/forms/AccountProfile";
 
 async function Page() {
   const user = await currentUser();
   if (!user) return null; // to avoid typescript warnings
 
-  const userInfo = await fetchUser(user.id);
-  if (userInfo?.onboarded) redirect("/");
+  // const userInfo = await fetchUser(user.id);
+  // if (userInfo?.onboarded) redirect("/");
 
   const userData = {
     id: user.id,
-    objectId: userInfo?._id,
-    username: userInfo ? userInfo?.username : user.username,
-    name: userInfo ? userInfo?.name : user.firstName ?? "",
-    bio: userInfo ? userInfo?.bio : "",
-    image: userInfo ? userInfo?.image : user.imageUrl,
+    objectId: '1',
+    username: "nitesh",
+    name: "nitesh_meena",
+    bio:"hello i am nitesh meena",
+    image: user.imageUrl,
   };
 
   return (
