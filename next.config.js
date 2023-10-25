03cc805aaa1027a129fc741ddeb21121dc2c33b1
nitesh <niteshmeena9672@gmail.com>
2023-10-25 13:30:02 +0530

    NextJs config update

--- next.config.js
+++ next.config.js
@@ -1,37 +1,34 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      serverActions: true,
-      serverComponentsExternalPackages: ["mongoose"],
-    },
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "img.clerk.com",
-        },
-        {
-          protocol: "https",
-          hostname: "images.clerk.dev",
-        },
-        {
-          protocol: "https",
-          hostname: "uploadthing.com",
-        },
-        {
-          protocol: "https",
-          hostname: "placehold.co",
-        },
-        {
-          protocol: "https",
-          hostname: "utfs.io",
-        },
-      ],
-      typescript: {
-        ignoreBuildErrors: true,
+  experimental: {
+    serverActions: true,
+    serverComponentsExternalPackages: ["mongoose"],
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
       },
-    },
-  };
-  
-  
-  module.exports = nextConfig;
+      {
+        protocol: "https",
+        hostname: "images.clerk.dev",
+      },
+      {
+        protocol: "https",
+        hostname: "uploadthing.com",
+      },
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
